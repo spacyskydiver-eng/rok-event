b8543ce22feaa62b039b7e5409ca4e6f8f5520bf
Alex Taylor <alextaylor@RichardMacmini.local>
2026-01-31 15:26:08 +0000

    Version 5.3.7

--- components/tech-tree/TechTree.tsx
+++ components/tech-tree/TechTree.tsx
@@ -6,8 +6,8 @@ import type { TechNode } from '@/lib/tech-tree/types'
 
 const NODE_WIDTH = 240
 const NODE_HEIGHT = 100
-const OFFSET_X = 200   // move right
-const OFFSET_Y = 120   // move down
+const OFFSET_X = 400   // move right
+const OFFSET_Y = 0   // move down
 
 type TechTreeProps = {
   title: string
